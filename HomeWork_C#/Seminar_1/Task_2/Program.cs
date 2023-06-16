42b4511643aaa61bba0a6ee338e4e1d0bedb813f
Mariya Gurashova <Gurash87@yandex.ru>
2023-06-16 12:10:30 +0300

    task_2

--- HomeWork_C#/Seminar_1/Task_2/Program.cs
+++ HomeWork_C#/Seminar_1/Task_2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+*/
+
+
+int Max(int args1, int args2, int args3)
+{
+    int resalt = 0;
+    if (args1 > resalt) resalt = args1;
+    if (args2 > resalt) resalt = args2;
+    if (args3 > resalt) resalt = args3;
+    return resalt;
+}
+
+int[] array = {50,89,126};
+
+int resalt = Max(array[0], array[1], array[2]);
+
+Console.WriteLine(resalt);
+
